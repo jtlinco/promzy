fd2873f95f24ea16bb0575b298b287892774cade
Jesse Torgbe-Cofie <bohan3d@gmail.com>
2012-12-26 12:11:31 +0000

    Base template complete
    Still a few structural changes to make

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+		var $slNav = $('.arrow a');
+		
+		$slNav.hover(slNavOver, slNavOut);
+ })
---
+++ js/plugins.js
@@ -0,0 +1,35 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function noop() {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+function slNavOver() {
+	var $imgs = $(this).find('img');
+	$($imgs[0]).hide();
+	$($imgs[1]).fadeIn(1000);
+}
+
+function slNavOut() {
+	var $imgs = $(this).find('img');
+	$($imgs[1]).hide();
+	$($imgs[0]).fadeIn(1000);
+}
